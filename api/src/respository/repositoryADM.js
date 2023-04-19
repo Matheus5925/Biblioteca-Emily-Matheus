9c243d8beb7f9191a4f2b4b68de0a93d76535e12
Matheus5925 <mfagundesfernandes0617@outlook.com>
2023-04-18 22:50:41 -0300

    ajust

--- api/src/respository/repositoryADM.js
+++ api/src/respository/repositoryADM.js
@@ -1,14 +1,14 @@
 import {con} from './connection.js';
 
 class Administrador{
-    async LoginAdm(email, senha){
+    async LoginAdm(email, password){
         const comando = `
         SELECT DS_EMAIL, DS_SENHA, NM_USUARIO
         FROM TB_USUARIO
         WHERE DS_EMAIL = ? and
             DS_SENHA = ?`
 
-        const [registro] = await (await con).query(comando, [email, senha]);
+        const [registro] = await (await con).query(comando, [email, password]);
         return registro[0];
     }
 }
