1963a4a7005ce4d544f00d5887562b0bfb5384af
Matheus5925 <mfagundesfernandes0617@outlook.com>
2023-04-19 22:49:55 -0300

    dsadasdasdasdasdas

--- api/src/controller/controllerBooks.js
+++ api/src/controller/controllerBooks.js
@@ -56,16 +56,51 @@ server.get('/books/name', async (req, resp) =>{
     }
 });
 
-server.post("/addbook", async (req, res) => {
+server.post("/addbook", async (req, resp) => {
     try {
-        const infoBook = req.body;
+        const infoBook = req.body
 
-        if(!infoBook)
-            throw new Error('')
+        if(!infoBook.idUsuario)
+            throw new Error('id user not informed!')
+    
+        if(!infoBook.nameBook)
+            throw new Error('Name not informed!')
+            
+        if(!infoBook.insbn)
+            throw new Error('INSBN not informed!')
+
+        if(!infoBook.editora)
+            throw new Error('Publish Company not informed!')
+
+        if(!infoBook.edition)
+            throw new Error('Edition not informed! ')
+
+        if(!infoBook.synopsis)
+            throw new Error('Synopsis not informed!')
+
+        if(!infoBook.publication)
+            throw new Error('Publication not informed!')
+
+        if(!infoBook.language)
+            throw new Error('Language not informed!')
+
+        if(!infoBook.available)
+            throw new Error('Available not informed!')
+
+        if(!infoBook.amountPage)
+            throw new Error('Amout Page not informed!')
+
+        if(!infoBook.price)
+            throw new Error('Price not informed!')
+
 
         const result = await book.AddBookDatabase(infoBook);
+
+        resp.send(result)
     } catch (err) {
-        
+        resp.status(400).send({
+            error: err.message
+        })
     }
 })
 
